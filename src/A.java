6d2ccd8b8a4a4359cbedcf377f446f6f9e653132
japanesebonobo <tomi119119@gmail.com>
2020-06-13 01:24:54 +0900

    Update A

---
+++ src/A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String S = scanner.next();
+
+        if (S.equals("ABC")) {
+            System.out.println("ARC");
+        } else {
+            System.out.println("ABC");
+        }
+    }
+}
